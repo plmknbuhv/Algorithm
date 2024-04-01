ee2d2128fa7c84edbbaf1a73338fa61bc876323f
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-04-01 14:56:50 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 288 ms, Memory: 6576 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = -1; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
