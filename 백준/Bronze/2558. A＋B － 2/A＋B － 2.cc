5e40dd4d43a034eec08c75d54b4a48e0b988f4ab
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-04-02 08:26:55 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+
+    int a[2];
+
+    scanf("%d", &a[0]);
+    scanf("%d", &a[1]);
+
+    printf("%d", a[0] + a[1]);
+}
