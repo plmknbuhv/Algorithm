2f2aabdb3348000d652cb2569c062fe19f0c09ff
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-04-06 19:50:01 +0900

    [Bronze V] Title: 개, Time: 60 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|\n");
+        }
+    }
+}
