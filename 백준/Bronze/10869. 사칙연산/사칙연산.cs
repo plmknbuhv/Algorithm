a2b11bada7c7755be63069e84604ad277306f846
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-04-08 08:32:55 +0900

    [Bronze V] Title: 사칙연산, Time: 64 ms, Memory: 5524 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baekjoon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = Console.ReadLine().Split();
+            Console.WriteLine(int.Parse(input[0]) + int.Parse(input[1]));
+            Console.WriteLine(int.Parse(input[0]) - int.Parse(input[1]));
+            Console.WriteLine(int.Parse(input[0]) * int.Parse(input[1]));
+            Console.WriteLine(int.Parse(input[0]) / int.Parse(input[1]));
+            Console.WriteLine(int.Parse(input[0]) % int.Parse(input[1]));
+        }
+    }
+}
