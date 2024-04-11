7cd9987541d2c1cb4a19f8abc5db790bdd9f9a17
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-04-11 14:54:37 +0900

    [Bronze V] Title: A+B - 8, Time: 72 ms, Memory: 6188 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] b = new int[a * 2];
+            int[] c = new int[a];
+            for (int i = 0; i < a * 2; i += 2)
+            {
+                c = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                b[i] = c[0];
+                b[i + 1] = c[1];
+            }
+            for (int i = 0; i < a * 2; i += 2)
+            {
+                Console.WriteLine($"Case #{(i / 2) + 1}: {b[i]} + {b[i + 1]} = {b[i] + b[i + 1]}");
+            }
+        }
+    }
+}
