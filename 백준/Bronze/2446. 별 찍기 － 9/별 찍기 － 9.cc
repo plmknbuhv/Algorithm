da99ca7eba113e11869b141fba8863b0343493df
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-04-22 10:01:50 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <ctime>
+#include <string.h>
+
+int main(void)
+{
+    int a;
+
+    scanf("%d", &a);
+
+    for (int i = 0; i < a; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = 1; j < (a - i) * 2; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 0, b = a - 1;  i < b; i++)
+    {
+        for (int j = 1; j < b - i; j++)
+        {
+            printf(" ");
+        }
+        for (int j = -2; j <= i * 2; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}   
