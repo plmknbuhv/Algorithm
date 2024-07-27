d7e5804c1a7bfb57ffe0491cfc6ab9f940c7fbb4
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-07-27 16:29:41 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string str;
+
+    while(true) {
+        getline(cin, str);
+        if(str == "") break;
+
+        cout << str << '\n';
+    }
+
+    return 0;
+}
