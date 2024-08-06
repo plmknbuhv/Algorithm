53bfbf7478894438a734f46603a9ecadfdb22231
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-08-06 23:42:46 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    cin >> e;
+
+    cout << a + b + c + d + e << endl
+    ;
+}
+
