dd36121e8349d571896b1dd962163c9d1c98e4f2
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-09-02 22:10:28 +0900

    [Bronze V] Title: CASIO, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    long long input;
+    long long input2;
+    cin >> input >> input2;
+
+    if (input2 == input)
+    {
+        cout << 1;
+    }
+    else
+    {
+        cout << 0;
+    }
+
+    return 0;
+}
