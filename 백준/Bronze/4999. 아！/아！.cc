884f6bbd831d4b5350c5f1184916405613d8904a
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-10-07 19:28:44 +0900

    [Bronze V] Title: 아!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int result = 0;
+
+
+int main()
+{
+    string a, b;
+    cin >> a >> b;
+
+    if (a > b)
+        cout << "no";
+    else
+        cout << "go";
+
+    return 0;
+}
