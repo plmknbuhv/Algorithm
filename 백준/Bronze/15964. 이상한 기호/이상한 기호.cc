2372e0e8976741344ff9ac195768541667c76c0c
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-10-18 11:25:06 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long long input1, input2;
+
+    cin >> input1 >> input2;
+
+    cout << (input1 + input2) * (input1 - input2);
+
+
+    return 0;
+}
