fdf0b7651d5f70f8b3001b3a899a2aa55fa9ce57
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-11-10 14:29:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    long long num;
+
+    cin >> num;
+
+    cout << (num - 1) * num / 2 << endl;
+    cout << 2;
+}
