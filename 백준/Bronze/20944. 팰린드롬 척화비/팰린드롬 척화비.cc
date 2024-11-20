163d83f423f96be4af57333085bcf569bd81e487
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-11-21 08:40:27 +0900

    [Bronze III] Title: 팰린드롬 척화비, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20944. 팰린드롬 척화비/팰린드롬 척화비.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int input;
+
+    cin >> input;
+    for (int i = 0; i < input; ++i)
+    {
+        cout << "a";
+    }
+    
+    return 0;
+}
