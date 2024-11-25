9b779a03168016f6531c7e0a3e12077961dfcf68
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-11-25 23:23:44 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <valarray>
+
+using namespace std;
+
+int main() {
+    int num;
+
+    cin >> num;
+
+    for (int i = 0; i < num; ++i)
+    {
+        cout << "LoveisKoreaUniversity" << " ";
+    }
+
+    return 0;
+}
