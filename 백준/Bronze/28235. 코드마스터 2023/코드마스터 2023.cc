6e858fc554be070b9554348a69a7966e3de6daa6
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2024-12-28 08:54:40 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+    if (str[0] == 'S') cout << "HIGHSCHOOL";
+    if (str[0] == 'C') cout << "MASTER";
+    if (str[0] == '2') cout << "0611";
+    if (str[0] == 'A') cout << "CONTEST";
+}
