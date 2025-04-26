292c822ed080a9d2c1d65ef221d677a72d8a93ab
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2025-04-26 14:32:26 +0900

    [Bronze V] Title: HI-ARC=?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28444. HI－ARC＝？/HI－ARC＝？.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <string.h>
+using namespace std;
+
+int main() {
+	int H, I, A, R, C;
+	cin >> H >> I >> A >> R >> C;
+
+	cout << (H * I) - (A * R * C);
+}
