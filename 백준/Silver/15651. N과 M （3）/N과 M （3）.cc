efb0d160e47964c51fe87ae6afc0916a880ebf0a
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2025-08-11 10:36:26 +0900

    [Silver III] Title: N과 M (3), Time: 344 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <utility>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int arr[100000];
+int N, M;
+
+void Check(int num, int cnt)
+{
+    arr[cnt] = num;
+    if (cnt == M-1)
+    {
+        for (int i = 0; i < cnt+1; i++)
+            cout << arr[i] << " ";
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 1; i <= N; i++)
+    {
+        Check(i, cnt + 1);
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> N >> M;
+
+    for (int i = 1; i <= N; i++)
+    {
+        Check(i, 0);
+    }
+}
