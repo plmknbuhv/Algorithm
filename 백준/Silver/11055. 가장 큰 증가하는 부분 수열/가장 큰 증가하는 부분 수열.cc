1b68760df3d43bba6234ff514286c19dadd29704
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2025-09-05 08:55:55 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <utility>
+#include <unordered_map>
+#include <set>
+#include <stack>
+#include <algorithm>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N, result = 1;
+    int arr[1001];
+    int memo[1001];
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    memo[0] = arr[0];
+    result = arr[0];
+
+    for (int i = 1; i < N; i++)
+    {
+        int temp = 0;
+        for (int j = 0; j < i; j++)
+        {
+            if (arr[i] > arr[j] && temp < memo[j])
+                temp = memo[j];
+        }
+        memo[i] = temp + arr[i];
+
+        if (result < memo[i])
+            result = memo[i];
+    }
+
+    cout << result;
+}
