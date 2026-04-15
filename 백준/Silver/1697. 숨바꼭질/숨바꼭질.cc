af745dea6785b60e99e9b50ee9362fc94b91f705
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2026-04-16 07:39:15 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2256 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,50 +1,51 @@
 #include <iostream>
-#include <utility>
-#include <algorithm>
+#include <vector>
 #include <queue>
-#include <math.h>
+
 using namespace std;
+
+bool visit[100001] = {};
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	int N, K;
+	cin >> N >> K;
 
-    int N, K;
-    bool visit[100001] = {};
-    queue<int> que;
-    queue<int> que2;
-    cin >> N >> K;
-    que.push(N);
-    que2.push(0);
+	queue<int> que;
+	queue<int> cntQue;
+	que.push(N);
+	cntQue.push(0);
 
-    while (que.empty() == false)
-    {
-        int temp = que.front();
-        int temp2 = que2.front();
-        que.pop();
-        que2.pop();
-        if (temp == K)
-        {
-            cout << temp2;
-            break;
-        }
+	while (que.empty() == false)
+	{
+		int num = que.front();
+		int cnt = cntQue.front();
+		que.pop();
+		cntQue.pop();
 
-        if (temp + 1 < 100001 && visit[temp + 1] == false)
-        {
-            que.push(temp + 1); que2.push(temp2+1);
-            visit[temp + 1] = true;
-        }
-        if (temp - 1 >= 0 && visit[temp - 1] == false)
-        {
-            que.push(temp - 1); que2.push(temp2+1);
-            visit[temp - 1] = true;
-        }
-        if (temp * 2 < 100001 && visit[temp*2] == false)
-        {
-            que.push(temp * 2); que2.push(temp2+1);
-            visit[temp*2] = true;
-        }
-    }
+		if (num == K)
+		{
+			cout << cnt;
+			break;
+		}
+
+		if (num - 1 >= 0 && visit[num - 1] == false)
+		{
+			visit[num - 1] = true;
+			que.push(num - 1);
+			cntQue.push(cnt+1);
+		}
+		if (num + 1 < 100001 && visit[num + 1] == false)
+		{
+			visit[num + 1] = true;
+			que.push(num + 1);
+			cntQue.push(cnt + 1);
+		}
+		if (num * 2 < 100001 && visit[num * 2] == false)
+		{
+			visit[num * 2] = true;
+			que.push(num * 2);
+			cntQue.push(cnt + 1);
+		}
+	}
 }
