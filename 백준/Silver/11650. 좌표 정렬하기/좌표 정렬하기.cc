ac49f2bbb1d247bef2a138e21df400e36c5a31fa
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2026-04-18 20:32:19 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 40 ms, Memory: 2804 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.cc
@@ -1,52 +1,40 @@
 #include <iostream>
-#include <utility>
 #include <vector>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-typedef pair<int,int> Vector2;
+pair<int, int> arr[100001];
 
-int compare(const void* a, const void* b)
+bool cmp(pair<int, int> numPair1, pair<int, int> numPair2)
 {
-    int x1 = ((Vector2*)(a))->first;
-    int y1 = ((Vector2*)(a))->second;
-    int x2 = ((Vector2*)(b))->first;
-    int y2 = ((Vector2*)(b))->second;
-
-    if (x1 < x2)
-        return -1;
-    else if (x1 == x2)
-        if (y1 < y2)
-            return -1;
-
-    return 1;
+	if (numPair1.first != numPair2.first)
+		return numPair1.first < numPair2.first;
+	else
+		return numPair1.second < numPair2.second;
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int count;
-    cin >> count;
+	int N;
+	cin >> N;
 
-    vector<Vector2> arr = {};
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr[i].first >> arr[i].second;
+	}
 
-    for (int i = 0; i < count; ++i)
-    {
-        int temp1;
-        int temp2;
-        cin >> temp1 >> temp2;
-        arr.emplace_back(temp1,temp2);
-    }
-
-    qsort(&arr[0], arr.size(), sizeof(arr[0]), compare);
+	sort(arr, arr+N, cmp);
 
 
-    for (int i = 0; i < count; ++i)
-    {
-        cout << arr[i].first << " " << arr[i].second << "\n";
-    }
+	for (int i = 0; i < N; i++)
+	{
+		cout << arr[i].first << " " << arr[i].second << '\n';
+	}
 }
+
