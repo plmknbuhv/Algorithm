ce0f24fa500fea2ec1bfebdc0fdaad211126a235
plmknbuhv <58427063+plmknbuhv@users.noreply.github.com>
2026-04-27 23:22:03 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,33 +1,39 @@
 #include <iostream>
-#include <utility>
-#include <algorithm>
+#include <vector>
 #include <queue>
-#include <math.h>
+#include <algorithm>
+#include <map>
+
 using namespace std;
+
+int N;
+int arr[10001] = {};
+int DP[10001][3] = {};
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	cin >> N;
 
-    int cnt;
-    cin >> cnt;
-    int* arr = new int[cnt];
-    int arr2[1000][3];
+	for (int i = 1; i <= N; i++)
+		cin >> arr[i];
 
-    cin >> arr[0];
-    arr2[0][0] = 0;
-    arr2[0][1] = arr[0];
-    arr2[0][2] = arr[0];
+	DP[0][0] = 0;
+	DP[0][1] = 0;
+	DP[0][2] = 0;
+	DP[1][0] = arr[1];
+	DP[1][1] = 0;
+	DP[1][2] = 0;
 
-    for (int i = 1; i < cnt; i++)
-    {
-        cin >> arr[i];
-        arr2[i][0] = max(arr2[i-1][1], arr2[i - 1][2]);
-        arr2[i][1] = arr2[i - 1][0] + arr[i];
-        arr2[i][2] = arr2[i - 1][1] + arr[i];
-    }
+	for (int i = 2; i <= N; i++)
+	{
+		DP[i][0] = max(DP[i - 2][0], DP[i - 2][1]) + arr[i];
+		DP[i][1] = DP[i - 1][0] + arr[i];
+	}
 
-    cout << max(arr2[cnt-1][1], arr2[cnt - 1][2]);
+	cout << max(max(DP[N][0], DP[N][1]), DP[N][2]);
 }
+			
